ca1fb7f60fa4e8127a9f0e7c150423b74732b453
Ap13ayush <111946055+Ap13ayush@users.noreply.github.com>
2023-06-27 17:36:02 +0530

    Add files via upload

---
+++ testTwo.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] argv) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();
+        
+        char firstNonRepeatingChar = getFirstNonRepeatingCharacter(inputString);
+        if (firstNonRepeatingChar != ' ') {
+            System.out.println("First non-repeating character: " + firstNonRepeatingChar);
+        } else {
+            System.out.println("-1");
+        }
+        
+        scanner.close();
+    }
+    
+    public static char getFirstNonRepeatingCharacter(final String inputString) {
+        int count[] = new int[256];
+        char inputChar[] = inputString.toCharArray();
+        for (char c : inputChar) {
+            count[(int) c]++;
+        }
+
+        for (char c : inputChar) {
+            if (count[(int) c] == 1) {
+                return c;
+            }
+        }
+        return ' ';
+    }
+}
